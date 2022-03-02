f8e3e45d26b82624f8cef72135176f74adf14b07
Matheus <monteiromatheus047@gmail.com>
2022-03-02 16:23:04 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace InverterString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringTeste = "InvertaIsso";
+            string StringInvertida = InverterString(stringTeste);
+
+            Console.Write("string original: " + stringTeste + Environment.NewLine);
+            Console.Write("string invertida: " + StringInvertida);
+
+            Console.ReadLine();
+        }
+        private static string InverterString(string stringTeste)
+        {
+            string StringInvertida = "";
+            for (int contador = stringTeste.Length - 1; contador >= 0; contador--)
+            {
+                StringInvertida += stringTeste[contador];
+            }
+            return StringInvertida;
+        }
+    }
+}
